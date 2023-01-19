bb8933cad30c28806c5bb928996448b264736423
Pavel <elemir2010@gmail.com>
2023-01-19 09:49:37 +0300

    add solution for tasks 2,4,6,8

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, 
+// которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+string? inputNum = Console.ReadLine(); // Считываем данные с консоли
+if(inputNum!=null)
+{
+    int inputNumber1 = int.Parse(inputNum); // парсим введеное число
+    string Result = string.Empty;
+    for(int i = 2; i < inputNumber1; i = i + 2)
+    {
+        Result = Result + i + ","; //
+    }
+    if(inputNumber1%2 == 0)
+    {
+        Result = Result + inputNumber1;
+    }
+    Console.WriteLine(Result); //Выводим данные в консоль
+}
+
