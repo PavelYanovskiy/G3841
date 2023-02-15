13dfce341b255e1d27008a9eda4c39efe0912842
Pavel <elemir2010@gmail.com>
2023-02-15 21:53:01 +0300

    add solutions for tasks 64,66,68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печатаем результат
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int RecSumMN(int m, int n)
+{
+    if (m >= n) return n; // точка остановки происходит когда минимальное число будет равно максимальномоу
+    return m + RecSumMN(m + 1, n); //вызываем метод RecSumMN и прибавляем к минимальному значению 1
+}
+
+
+int m = ReadData("Введите значение числа M:");
+int n = ReadData("Введите значение числа N:");
+int res = m < n ? RecSumMN(m, n): RecSumMN(n, m);
+PrintResult(res);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintResult(int prefix)
+{
+    Console.Write(prefix);
+}
+int Ackermann(int m, int n) //метод вычисления функции Аккермана
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return Ackermann(m - 1, 1);
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+
+}
+
+int m = ReadData("Введите значение числа M:");
+int n = ReadData("Введите значение числа N:");
+int a = Ackermann(m, n);
+Console.Write("Результат: ");
+PrintResult(a);
+Console.WriteLine();
+
